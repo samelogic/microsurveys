4b23b77095c5a345869d371263c24ed934622d33
Shawn Mclean <shawnrmclean@gmail.com>
2022-06-22 21:44:59 -0500

    chore: add types to main export

--- packages/react-microsurveys/src/index.ts
+++ packages/react-microsurveys/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/ReactMicrosurveys';
+export * from '@samelogic/microsurveys-types';
 export * from '@samelogic/react-microsurveys-client';
 export * from '@samelogic/react-microsurveys-editor';
