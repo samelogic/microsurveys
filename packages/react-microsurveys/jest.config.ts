6c276e96267559ce8033a536e964d6eb120aaaeb
Shawn Mclean <shawnrmclean@gmail.com>
2022-07-13 17:01:14 -0500

    chore: switch swc to babel

--- packages/react-microsurveys/jest.config.ts
+++ packages/react-microsurveys/jest.config.ts
@@ -3,10 +3,7 @@ export default {
   displayName: 'react-microsurveys',
   preset: '../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]sx?$': [
-      '@swc/jest',
-      { jsc: { transform: { react: { runtime: 'automatic' } } } },
-    ],
+    '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/react-microsurveys',
