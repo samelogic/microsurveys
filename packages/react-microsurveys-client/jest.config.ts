4db1c9351f6f4eb842a512d85e45a439d317aacb
Shawn Mclean <shawnrmclean@gmail.com>
2022-07-13 17:28:47 -0500

    refactor: update build to rollup

--- packages/react-microsurveys-client/jest.config.ts
+++ packages/react-microsurveys-client/jest.config.ts
@@ -3,10 +3,7 @@ export default {
   displayName: 'react-microsurveys-client',
   preset: '../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]sx?$': [
-      '@swc/jest',
-      { jsc: { transform: { react: { runtime: 'automatic' } } } },
-    ],
+    '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/react-microsurveys-client',
