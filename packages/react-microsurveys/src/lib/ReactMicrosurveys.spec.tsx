68f947af143de6fc8711c9b0c8388770fc376903
Shawn Mclean <shawnrmclean@gmail.com>
2022-08-01 19:29:51 -0500

    style: fix styles

--- packages/react-microsurveys/src/lib/ReactMicrosurveys.spec.tsx
+++ packages/react-microsurveys/src/lib/ReactMicrosurveys.spec.tsx
@@ -1,7 +1,3 @@
-import { render } from '@testing-library/react';
-
-import ReactMicrosurveys from './ReactMicrosurveys';
-
 describe('ReactMicrosurveys', () => {
   it('should render successfully', () => {
     // const { baseElement } = render(<ReactMicrosurveys />);
