47a66578ab988909fde7962085b80fd117c6c329
Shawn Mclean <shawnrmclean@gmail.com>
2022-08-04 17:33:53 -0500

    chore: cleanup webpack file

--- packages/microsurveys-step/webpack.config.js
+++ packages/microsurveys-step/webpack.config.js
@@ -1,10 +1,5 @@
-// https://github.com/nrwl/nx/issues/3175
-const nrwlConfig = require('@nrwl/react/plugins/webpack.js');
-
 // https://stackoverflow.com/questions/68707553/uncaught-referenceerror-buffer-is-not-defined
 module.exports = (config, context) => {
-  const newConfig = nrwlConfig(config);
-
   config = {
     ...config,
     mode: 'production',
@@ -23,8 +18,6 @@ module.exports = (config, context) => {
       },
     },
   };
-
-  console.log(config);
 
   return config;
 };
