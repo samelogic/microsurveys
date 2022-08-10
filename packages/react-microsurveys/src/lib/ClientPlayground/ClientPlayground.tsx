baf4b49402b8006ed3fef61e8285dcfea5fea22d
Shawn Mclean <shawnrmclean@gmail.com>
2022-08-10 12:16:29 -0500

    refactor: move into client playground

---
+++ packages/react-microsurveys/src/lib/ClientPlayground/ClientPlayground.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { MicrosurveyClient } from '@samelogic/react-microsurveys-client';
+import { Form, Response } from '@samelogic/microsurveys-types';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+
+export interface ClientPlaygroundProps {
+  form: Form;
+}
+
+export function ClientPlayground({ form }: ClientPlaygroundProps) {
+  const [responseData, setResponseData] = useState<Response>();
+  const handleClientSubmit = (response: Response) => {
+    setResponseData(response);
+  };
+  return (
+    <Grid container item justifyContent="center">
+      Mode: {form.type}
+      <MicrosurveyClient form={form} onSubmit={handleClientSubmit} />
+      {responseData && (
+        <Grid item justifyContent="center" xs={12}>
+          <Typography variant="h2" sx={{ mb: 2 }}>
+            Response
+          </Typography>
+          <Typography color="text.secondary" sx={{ mb: 4 }}>
+            Example of what you would see if you submitted the form.
+          </Typography>
+          <Paper variant="outlined" sx={{ m: 2, p: 2 }}>
+            <pre>{JSON.stringify(responseData, null, 2)}</pre>
+          </Paper>
+        </Grid>
+      )}
+    </Grid>
+  );
+}
+
+export default ClientPlayground;
